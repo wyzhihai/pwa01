61d066620c7178b869b999bfc0612aa043609b56
liuqun <1484772686@qq.com>
2021-03-30 11:19:27 +0800

    fixbug

--- sw.js
+++ sw.js
@@ -16,30 +16,30 @@ self.addEventListener('install', event => {
 });
 
 // Cache any new resources as they are fetched
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request, { ignoreSearch: true })
-//     .then(function(response) {
-//       if (response) {
-//         return response;
-//       }
-//       var fetchRequest = event.request.clone();
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true })
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+      var fetchRequest = event.request.clone();
 
-//       return fetch(fetchRequest).then(
-//         function(response) {
-//           if(!response || response.status !== 200) {
-//             return response;
-//           }
+      return fetch(fetchRequest).then(
+        function(response) {
+          // if(!response || response.status !== 200) {
+          //   return response;
+          // }
 
-//           var responseToCache = response.clone();
-//           caches.open(cacheName)
-//           .then(function(cache) {
-//             cache.put(event.request, responseToCache);
-//           });
+          // var responseToCache = response.clone();
+          // caches.open(cacheName)
+          // .then(function(cache) {
+          //   cache.put(event.request, responseToCache);
+          // });
 
-//           return response;
-//         }
-//       );
-//     })
-//   );
-// });
+          return response;
+        }
+      );
+    })
+  );
+});
